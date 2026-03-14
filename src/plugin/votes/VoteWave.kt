ed15a435199fed6fae43d920c36a5a0a372d4823
kuko <262641371+kukoldiki@users.noreply.github.com>
2026-03-14 16:55:39 +0500

    Вышел из запоя, сделал vnw

---
+++ src/plugin/votes/VoteWave.kt
@@ -0,0 +1,58 @@
+package plugin.votes
+
+import arc.struct.ObjectIntMap
+import arc.util.Timer
+import mindustry.Vars
+import mindustry.gen.Groups
+import mindustry.gen.Player
+import plugin.Bundle
+import plugin.PVars.waveVote
+import kotlin.math.max
+import kotlin.math.roundToInt
+
+class VoteWave {
+    var voted: ObjectIntMap<String> = ObjectIntMap()
+    var votes = 0
+    var task: Timer.Task? = null
+
+    constructor() {
+        task = Timer.schedule({
+            if(!checkPass()) {
+                Bundle.sendMessage("vnw.failed")
+                cancel()
+            }
+        }, 30f)
+    }
+
+    fun vote(player: Player, d: Int) {
+        votes += d
+        voted.put(player.ip(), d)
+
+        if(d == 1)
+            Bundle.sendMessage("vnw.votedy", player.coloredName())
+        else
+            Bundle.sendMessage("vnw.votedn", player.coloredName())
+
+        checkPass()
+    }
+
+    fun checkPass(): Boolean {
+        if(votes >= votesRequired()) {
+            Vars.logic.runWave()
+            cancel()
+
+            Bundle.sendMessage("vnw.pass")
+            return true
+        }
+        return false
+    }
+
+    fun cancel() {
+        task?.cancel()
+        waveVote = null
+    }
+
+    fun votesRequired(): Int {
+        return max(1, (Groups.player.size() * 0.75).roundToInt())
+    }
+}
