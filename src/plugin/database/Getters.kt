f977d8b141711e32f8464a133d7d4498e5336e21
kuko <262641371+kukoldiki@users.noreply.github.com>
2026-03-15 21:40:39 +0500

    2. Ban, Log, PlayerData

--- src/plugin/database/Getters.kt
+++ src/plugin/database/Getters.kt
@@ -1,17 +1,30 @@
 package plugin.database
 
 import arc.struct.ObjectMap
+import arc.util.Strings
+import mindustry.gen.Groups
 import mindustry.gen.Player
-import plugin.database.models.Admin
+import mindustry.net.Administration
+import mindustry.net.Administration.PlayerAction
+import plugin.PVars
+import plugin.database.Database.StatementSetter
+import plugin.database.models.*
 import plugin.utils.Permission
+import plugin.utils.getUDPAddress
 import java.sql.PreparedStatement
 import java.sql.ResultSet
 import java.sql.SQLException
+import java.sql.Timestamp
+import java.time.Instant
 import java.util.*
 import java.util.function.Consumer
 
 @JvmField
 var adminsCache = ObjectMap<Player, Admin>()
+@JvmField
+var playerDataCache = ObjectMap<Player, PlayerData>()
+
+// region Admin
 
 fun updateAdminHidden(pid: Int, hidden: Boolean): Boolean {
     return Database.executeUpdate(
@@ -59,3 +72,320 @@ fun getAdmin(rs: ResultSet): Admin {
         rs.getBoolean("hidden")
     )
 }
+
+// endregion
+
+// region Ban
+
+fun ban(pid: Int, aid: Int, reason: String?, unban: Long): Boolean {
+    return Database.executeUpdate(
+        "INSERT INTO bans (player_id, admin_id, reason, unban_time) VALUES (?, ?, ?, ?)",
+        StatementSetter { stmt: PreparedStatement? ->
+            stmt!!.setInt(1, pid)
+            stmt.setInt(2, aid)
+            stmt.setString(3, reason)
+            stmt.setTimestamp(4, getTimestamp(unban))
+        }
+    )
+}
+
+fun ban(pid: Int, admin: Player, reason: String?, unban: Long): Boolean {
+    return Database.executeUpdate(
+        """
+        INSERT INTO bans (player_id, admin_id, reason, unban_time)
+        VALUES (
+            ?,
+            (SELECT id FROM players WHERE uuid = ?),
+            ?,
+            ?
+        )
+    
+    """.trimIndent()
+    ) { stmt: PreparedStatement ->
+        stmt.setInt(1, pid)
+        stmt.setString(2, admin.uuid())
+        stmt.setString(3, reason)
+        stmt.setTimestamp(4, getTimestamp(unban))
+    }
+}
+
+fun ban(player: Player, admin: Player, reason: String?, unban: Long): Boolean {
+    return Database.executeUpdate(
+        """
+        INSERT INTO bans (player_id, admin_id, reason, unban_time)
+        VALUES (
+            (SELECT id FROM players WHERE uuid = ?),
+            (SELECT id FROM players WHERE uuid = ?),
+            ?,
+            ?
+        )
+    
+    """.trimIndent()
+    ) { stmt: PreparedStatement ->
+        stmt.setString(1, player.uuid())
+        stmt.setString(2, admin.uuid())
+        stmt.setString(3, reason)
+        stmt.setTimestamp(4, getTimestamp(unban))
+    }
+}
+
+fun getTimestamp(seconds: Long): Timestamp? {
+    if (seconds < 0) return null // перм бан
+
+
+    val millis = seconds * 1000
+    return Timestamp(System.currentTimeMillis() + millis)
+}
+
+fun getBan(id: Int): Optional<Ban> {
+    return Database.executeQueryAsync(
+        "SELECT * FROM bans WHERE id = ?",
+        { stmt: PreparedStatement -> stmt.setInt(1, id) },
+        { rs: ResultSet -> getBan(rs) }
+    )
+}
+
+fun getBan(player: Player): Optional<Ban> {
+    return Database.executeQueryAsync(
+        """
+SELECT b.*
+FROM bans b
+JOIN players p ON p.id = b.player_id
+WHERE b.active = true
+  AND (b.unban_time IS NULL OR b.unban_time > NOW())
+  AND (
+        p.uuid = ?
+        OR p.last_ip = ?
+        OR EXISTS (
+            SELECT 1
+            FROM usid_list ul
+            WHERE ul.player_id = p.id
+              AND ul.usid = ?
+              AND ul.server = ?
+        )
+  )
+LIMIT 1;
+                
+                """.trimIndent(),
+        { stmt: PreparedStatement ->
+            stmt.setString(1, player.uuid())
+            stmt.setString(2, player.ip())
+            stmt.setString(3, player.usid())
+            stmt.setInt(4, PVars.serverId)
+        },
+        { rs: ResultSet -> getBan(rs) }
+    )
+}
+
+@Throws(SQLException::class)
+fun getBan(rs: ResultSet): Ban {
+    val banId = rs.getInt("id")
+
+    val banTs = rs.getTimestamp("ban_time")
+    val unbanTs = rs.getTimestamp("unban_time")
+
+    val banTime = if (banTs != null) banTs.toInstant() else Instant.now()
+    val unbanTime = if (unbanTs != null) unbanTs.toInstant() else null
+
+    return Ban(
+        banId,
+        rs.getInt("player_id"),
+        rs.getInt("admin_id"),
+        rs.getBoolean("active"),
+        banTime,
+        unbanTime,
+        rs.getString("reason")
+    )
+}
+
+// endregion
+
+// region Log
+
+fun putLog(type: String?, message: String?) {
+    PVars.logsBuffer.add(Log(null, type, message))
+}
+
+fun putLog(pid: Int?, type: String?, message: String?) {
+    PVars.logsBuffer.add(Log(pid, type, message))
+}
+
+fun putLog(action: PlayerAction, pd: PlayerData) {
+    val message: String?
+    val type = action.type
+
+    val tile = action.tile ?: return
+    if (tile.block().isAir) return
+
+    when (type) {
+        Administration.ActionType.breakBlock -> message =
+            Strings.format("Player break block @ at @ @", tile.block().name, tile.x, tile.y)
+
+        Administration.ActionType.placeBlock -> message =
+            Strings.format("Player placed block @ at @ @", action.block, tile.x, tile.y)
+
+        Administration.ActionType.rotate -> message =
+            Strings.format("Player rotated block @ at @ @", tile.block().name, tile.x, tile.y)
+
+        Administration.ActionType.configure -> message =
+            Strings.format("Player configured block @ at @ @", tile.block().name, tile.x, tile.y)
+
+        else -> return
+    }
+
+    putLog(pd.id, "action", message)
+}
+
+// endregion
+
+// region PlayerData
+
+fun deepSearchNames(player: Player): MutableList<String> {
+    return Database.executeQueryList(
+        """
+                SELECT DISTINCT p.last_name
+                FROM players p
+                LEFT JOIN usid_list u ON u.player_id = p.id
+                LEFT JOIN connections c ON c.player_id = p.id
+                WHERE p.last_ip = ?
+                   OR u.usid = ?
+                   OR c.address = ?
+                
+                """.trimIndent(),
+        { stmt: PreparedStatement ->
+            stmt.setString(1, player.ip())
+            stmt.setString(2, player.usid())
+            stmt.setString(3, player.ip())
+        },
+        { rs: ResultSet -> rs.getString("last_name") }
+    )
+}
+
+fun deepSearch(player: Player): MutableList<PlayerData> {
+    return Database.executeQueryList(
+        """
+                SELECT DISTINCT p.*
+                FROM players p
+                LEFT JOIN usid_list u ON u.player_id = p.id
+                LEFT JOIN connections c ON c.player_id = p.id
+                WHERE p.last_ip = ?
+                   OR u.usid = ?
+                   OR c.address = ?
+                
+                """.trimIndent(),
+        { stmt: PreparedStatement? ->
+            stmt!!.setString(1, player.ip())
+            stmt.setString(2, player.usid())
+            stmt.setString(3, player.ip())
+        },
+        { rs: ResultSet -> getPlayerData(rs) }
+    )
+}
+
+fun getPlayerById(id: Int): Optional<Player> {
+    val pdOpt = getPlayerData(id)
+    var p: Player? = null
+    if (pdOpt.isPresent) {
+        val pd = pdOpt.get()
+        p = Groups.player.find { player: Player -> player.uuid() == pd.uuid }
+    }
+    return Optional.ofNullable<Player>(p)
+}
+
+fun getOrCreatePlayerData(p: Player): Optional<PlayerData> {
+    if (playerDataCache.containsKey(p)) {
+        return Optional.of(playerDataCache.get(p))
+    }
+
+    val pd = Database.executeQueryAsync(
+        """
+                        WITH update_players AS (
+                            INSERT INTO players (uuid, last_name, last_ip, locale, color)
+                            VALUES (?, ?, ?, ?, ?)
+                            ON CONFLICT (uuid) DO UPDATE SET
+                                last_name = EXCLUDED.last_name,
+                                last_ip   = EXCLUDED.last_ip,
+                                color     = EXCLUDED.color,
+                                locale    = EXCLUDED.locale,
+                                last_seen = NOW()
+                            RETURNING id, uuid, last_name, last_ip, locale, color, discord_id, prefs
+                        ),
+                        insert_usid AS (
+                            INSERT INTO usid_list (player_id, usid, server)
+                            SELECT id, ?, ?
+                            FROM update_players
+                            ON CONFLICT (usid, server) DO NOTHING
+                        ),
+                        insert_connection AS (
+                            INSERT INTO connections(player_name, address, address_udp, server_id, player_id)
+                            SELECT last_name, last_ip, ?, ?, id
+                            FROM update_players
+                        ),
+                        insert_stats AS (
+                            INSERT INTO statistics (player_id)
+                            SELECT id
+                            FROM update_players
+                            ON CONFLICT (player_id) DO NOTHING
+                        )
+                        SELECT *
+                        FROM update_players;
+                        
+                        """.trimIndent(),
+        { stmt: PreparedStatement ->
+            stmt.setString(1, p.uuid())
+            stmt.setString(2, p.name())
+            stmt.setString(3, p.ip())
+            stmt.setString(4, p.locale)
+            stmt.setString(5, p.color.toString())
+            stmt.setString(6, p.usid())
+            stmt.setInt(7, PVars.serverId)
+            stmt.setString(8, getUDPAddress(p))
+            stmt.setInt(9, PVars.serverId)
+        },
+        { rs: ResultSet -> getPlayerData(rs) }
+    )
+    if (!playerDataCache.containsKey(p) && pd.isPresent) playerDataCache.put(p, pd.get())
+    return pd
+}
+
+fun getPlayerData(id: Int): Optional<PlayerData> {
+    return Database.executeQueryAsync(
+        "SELECT * FROM players WHERE id = ?",
+        { stmt: PreparedStatement -> stmt.setInt(1, id) },
+        { rs: ResultSet -> getPlayerData(rs) }
+    )
+}
+
+fun getPlayerData(player: Player): Optional<PlayerData> {
+    if (playerDataCache.containsKey(player)) return Optional.of(playerDataCache.get(player))
+
+    return Database.executeQueryAsync(
+        "SELECT * FROM players WHERE uuid = ?",
+        { stmt: PreparedStatement -> stmt.setString(1, player.uuid()) },
+        { rs: ResultSet -> getPlayerData(rs) }
+    )
+}
+
+fun getPlayerId(player: Player): Optional<Int> {
+    if (playerDataCache.containsKey(player)) return Optional.of<Int>(playerDataCache.get(player).id)
+
+    return Database.executeQueryAsync(
+        "SELECT id FROM players WHERE uuid = ?",
+        { stmt: PreparedStatement -> stmt.setString(1, player.uuid()) },
+        { rs: ResultSet -> rs.getInt("id") }
+    )
+}
+
+@Throws(SQLException::class)
+fun getPlayerData(rs: ResultSet): PlayerData {
+    var prefs: PlayerPrefs?
+    try {
+        prefs = PVars.objectMapper.readValue(rs.getString("prefs"), PlayerPrefs::class.java)
+    } catch (e: Exception) {
+        prefs = PlayerPrefs()
+        arc.util.Log.err(e)
+    }
+    return PlayerData(rs.getInt("id"), rs.getString("uuid"), rs.getObject<Long?>("discord_id", Long::class.java), prefs)
+}
+
+// endregion
