9cd27c4a67c61cfa541048be553f08a6726f4834
kuko <262641371+kukoldiki@users.noreply.github.com>
2026-03-18 00:44:52 +0500

    delete map comman

--- src/plugin/discord/Commands.java
+++ src/plugin/discord/Commands.java
@@ -1,10 +1,12 @@
 package plugin.discord;
 
 import arc.Core;
+import arc.files.Fi;
 import arc.math.Mathf;
 import arc.struct.Seq;
 import arc.util.CommandHandler;
 import arc.util.CommandHandler.Command;
+import arc.util.Log;
 import arc.util.Strings;
 import arc.util.Timer;
 import com.zaxxer.hikari.HikariPoolMXBean;
@@ -13,6 +15,7 @@ import mindustry.gen.Groups;
 import mindustry.gen.Player;
 import mindustry.maps.Map;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.emoji.Emoji;
 import net.dv8tion.jda.api.utils.FileUpload;
 import plugin.KVars;
@@ -23,6 +26,7 @@ import plugin.utils.Permission;
 
 import java.awt.*;
 import java.util.Comparator;
+import java.util.Objects;
 
 import static plugin.PVars.linkCodes;
 import static plugin.PVars.needRestart;
@@ -168,7 +172,36 @@ public class Commands {
             msg.addFiles(FileUpload.fromData(map.file.file(), map.file.name()));
             //msg.addFiles(FileUpload.fromData(parseMap(map), "minimap.png"))
             map.file.delete();
+            Vars.maps.reload();
             msg.queue();
+        });
+
+        handler.<Context>register("uploadmap", "", (args, ctx)->{
+            if(!ctx.hasPerm(Permission.editMaps)) return;
+            Message msg = ctx.message;
+            Seq<Message.Attachment> attachments = new Seq<>();
+            msg.getAttachments().forEach(a->{
+                if(Objects.equals(a.getFileExtension(), Vars.mapExtension))
+                    attachments.add(a);
+            });
+            if(attachments.size > 4) {
+                ctx.reply("Too many maps!");
+                return;
+            }
+            attachments.each(map->{
+                Fi file = Vars.customMapDirectory.child(map.getFileName());
+                if(file.exists()) {
+                    ctx.reply("File "+file.name()+" already exists!");
+                    return;
+                }
+                map.getProxy().downloadToFile(file.file()).thenAccept(f->{
+                    ctx.reply("File "+file.name()+" downloaded!");
+                    Vars.maps.reload();
+                }).exceptionally(t->{
+                    Log.err("Failed to download map!", t);
+                    return null;
+                });
+            });
         });
 
         handler.<Context>register("restart", "SS", (a, ctx) -> {
