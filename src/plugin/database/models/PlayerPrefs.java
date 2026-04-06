47687e9a1282fba842975c987268cae8d33c979b
kuko <262641371+kukoldiki@users.noreply.github.com>
2026-04-07 02:13:54 +0500

    /name command

--- src/plugin/database/models/PlayerPrefs.java
+++ src/plugin/database/models/PlayerPrefs.java
@@ -2,8 +2,13 @@ package plugin.database.models;
 
 public class PlayerPrefs {
     public boolean showWelcomeMenu = true;
+    public String customName = "";
 
     public void setShowWelcomeMenu(boolean showWelcomeMenu) {
         this.showWelcomeMenu = showWelcomeMenu;
+    }
+
+    public void setCustomName(String s) {
+        this.customName = s;
     }
 }
