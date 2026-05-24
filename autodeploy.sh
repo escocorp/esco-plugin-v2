83c4f518b8ca5af1f5e29e4afb5e8b4870393a5d
kuko <262641371+kukoldiki@users.noreply.github.com>
2026-05-24 19:19:50 +0500

    nasral

--- autodeploy.sh
+++ autodeploy.sh
@@ -9,10 +9,10 @@ echo "Commit: $COMMIT"
 
 REMOTE_PATH="/www/builds/$COMMIT"
 
-ssh host "mkdir -p $REMOTE_PATH"
+ssh hostn "mkdir -p $REMOTE_PATH"
 
-scp build/libs/esco-plugin-v2.jar host:$REMOTE_PATH/plugin.jar
+scp build/libs/esco-plugin-v2.jar hostn:$REMOTE_PATH/plugin.jar
 
-ssh host "echo $COMMIT > /www/builds/latest.txt"
+ssh hostn "echo $COMMIT > /www/builds/latest.txt"
 
 echo "Done: $REMOTE_PATH (latest = $COMMIT)"
