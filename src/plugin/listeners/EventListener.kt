6e972f5eaf3421d8058872ec1d62db8b8a855e19
kuko <262641371+kukoldiki@users.noreply.github.com>
2026-05-29 18:57:27 +0500

    minor changes

---
+++ src/plugin/listeners/EventListener.kt
@@ -0,0 +1,5 @@
+package plugin.listeners
+
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.FUNCTION)
+annotation class EventListener()
---
+++ src/plugin/listeners/ListenerRegistrar.kt
@@ -0,0 +1,30 @@
+package plugin.listeners
+
+import arc.Events
+import kotlin.collections.get
+import kotlin.reflect.KClass
+import kotlin.reflect.full.hasAnnotation
+import kotlin.reflect.full.memberFunctions
+
+object ListenerRegistrar {
+
+    fun register(listener: Any) {
+        for (function in listener::class.memberFunctions) {
+
+            if (!function.hasAnnotation<EventListener>())
+                continue
+
+            if (function.parameters.size != 2)
+                continue
+
+            val eventParameter = function.parameters[1]
+            val eventClass =
+                eventParameter.type.classifier as? KClass<*>
+                    ?: continue
+
+            Events.on(eventClass.java) { event ->
+                function.call(listener, event)
+            }
+        }
+    }
+}
