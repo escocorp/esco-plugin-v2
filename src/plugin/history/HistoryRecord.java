ead9cd8713a490ac62b82bc427681b0278c0faa1
kuko <262641371+kukoldiki@users.noreply.github.com>
2026-05-30 17:00:09 +0500

    replays test

--- src/plugin/history/HistoryRecord.java
+++ src/plugin/history/HistoryRecord.java
@@ -6,7 +6,7 @@ import mindustry.world.Block;
 import java.util.Optional;
 
 public record HistoryRecord(String playerName, Optional<Integer> playerId, HistoryType type, Block block,
-                            UnitType unit, long time) {
+                            UnitType unit, long time, boolean center) {
     public String getMessage() {
         String actor = "[white]" + (playerName == null ?
                 unit == null ? "?" : unit.emoji() :
