f3677be6d6c1b39e6c407a19e9968697e3db968b
kuko <262641371+kukoldiki@users.noreply.github.com>
2026-05-30 18:24:29 +0500

    replays test

--- src/plugin/events/PEvents.kt
+++ src/plugin/events/PEvents.kt
@@ -335,7 +335,7 @@ fun loadEvents() {
                 name = player.coloredName()
                 pid = getPlayerId(player)
             }
-            History.write(tile, name, pid, HistoryType.buildBlock, tile.block(), unit.type(), tile.team(), tile.build.rotation)
+            History.write(tile, name, pid, HistoryType.buildBlock, tile.block(), unit.type(), tile.build.team, tile.build.rotation)
         }
     })
 
