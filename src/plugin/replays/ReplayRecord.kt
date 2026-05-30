b7066c48f70c68353d279d0ea5e9f85f10e262d6
kuko <262641371+kukoldiki@users.noreply.github.com>
2026-05-30 15:56:18 +0500

    replays test

--- src/plugin/replays/ReplayRecord.kt
+++ src/plugin/replays/ReplayRecord.kt
@@ -5,11 +5,11 @@ import java.util.Optional
 
 @Serializable
 data class ReplayRecord(
-    val playerName: String,
+    val playerName: String?,
     val playerId: Int?,
     val type: Int,
     val blockId: Short,
-    val unitId: Short,
+    val unitId: Short?,
     val time: Long
 ) {
 }
