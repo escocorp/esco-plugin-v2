d8cf54dc80990460b1e6d5aa0474feea14d02651
kuko <262641371+kukoldiki@users.noreply.github.com>
2026-06-10 15:52:45 +0500

    handle exceptions in coroutines

--- src/plugin/KVars.kt
+++ src/plugin/KVars.kt
@@ -1,13 +1,19 @@
 package plugin
 
+import arc.util.Log
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import plugin.database.models.MapStats
 
 object KVars {
-    val globalScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
-    val eventsScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+    val errorHandler = CoroutineExceptionHandler { _, e ->
+        Log.err("Exception in coroutine", e)
+    }
+
+    val globalScope = CoroutineScope(SupervisorJob() + Dispatchers.IO + errorHandler)
+    val eventsScope = CoroutineScope(SupervisorJob() + Dispatchers.IO + errorHandler)
 
     var mapStats: MapStats? = null
     var startTime = System.currentTimeMillis()
