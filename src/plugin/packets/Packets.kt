f95df8551e46470a00a6d22a49c1fdde65ccb9b8
kuko <262641371+kukoldiki@users.noreply.github.com>
2026-07-02 15:34:28 +0500

    convert Packets.java to Packets.kt

--- src/plugin/packets/Packets.kt
+++ src/plugin/packets/Packets.kt
@@ -1,26 +1,36 @@
-package plugin.packets;
+package plugin.packets
 
-import mindustry.Vars;
-import mindustry.gen.AdminRequestCallPacket;
-import mindustry.gen.Call;
-import mindustry.gen.SendChatMessageCallPacket;
-import mindustry.io.JsonIO;
+import arc.func.Cons2
+import mindustry.Vars
+import mindustry.gen.AdminRequestCallPacket
+import mindustry.gen.Call
+import mindustry.gen.Player
+import mindustry.gen.SendChatMessageCallPacket
+import mindustry.io.JsonIO
+import mindustry.net.NetConnection
+import plugin.PVars
 
-import static plugin.PVars.SSUsers;
+object Packets {
+    fun load() {
+        Vars.net.handleServer(
+            AdminRequestCallPacket::class.java,
+            Cons2 { con: NetConnection?, packet: AdminRequestCallPacket -> AdminRequest.handle(con, packet) })
+        Vars.net.handleServer(
+            SendChatMessageCallPacket::class.java,
+            Cons2 { con: NetConnection?, packet: SendChatMessageCallPacket -> SendChatMessage.handle(con, packet) })
 
-public class Packets {
-    public static void load() {
-        Vars.net.handleServer(AdminRequestCallPacket.class, AdminRequest::handle);
-        Vars.net.handleServer(SendChatMessageCallPacket.class, SendChatMessage::handle);
-
-        loadCustom();
+        loadCustom()
     }
 
-    private static void loadCustom() {
+    private fun loadCustom() {
         // scheme size integration
-        Vars.netServer.addPacketHandler("MySubtitle", (target, args) -> {
-            SSUsers.put(target.id, args);
-            Call.clientPacketReliable("Subtitles", JsonIO.write(SSUsers));
-        });
+        Vars.netServer.addPacketHandler("MySubtitle") { target: Player, args: String ->
+            PVars.SSUsers.put(target.id, args)
+            Call.clientPacketReliable("Subtitles", JsonIO.write(PVars.SSUsers))
+        }
+        // agzam mod
+        Vars.netServer.addBinaryPacketHandler("agzam4.cmd-sug") { player, bs ->
+
+        }
     }
 }
