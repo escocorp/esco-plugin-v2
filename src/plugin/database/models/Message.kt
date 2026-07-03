e42d7a970e98e9ab98e768c3e66cdd01ad59b76d
kuko <262641371+kukoldiki@users.noreply.github.com>
2026-07-03 05:37:02 +0500

    update

--- src/plugin/database/models/Message.kt
+++ src/plugin/database/models/Message.kt
@@ -5,6 +5,7 @@ Auto-generated on 2026-07-02T10:02:10.961Z
 Based on migrations.sql#L119 (table: messages)
 */
 import mindustry.gen.Player
+import plugin.PVars
 import plugin.database.Database.executeUpdate
 import kotlin.time.Instant
 import java.sql.ResultSet
@@ -35,14 +36,15 @@ fun getMessage(rs: ResultSet): Message {
 fun putMessage(playerId: Int, unformatted: String, formatted: String, timestamp: Instant): Boolean {
     return executeUpdate(
         """
-                INSERT INTO messages (player_id, unformatted, formatted, timestamp)
-                VALUES (?, ?, ?, ?)
+                INSERT INTO messages (player_id, unformatted, formatted, timestamp, server_id)
+                VALUES (?, ?, ?, ?, ?)
             """.trimIndent(),
         { stmt ->
             stmt.setInt(1, playerId)
             stmt.setString(2, unformatted)
             stmt.setString(3, formatted)
             stmt.setTimestamp(4, Timestamp.from(timestamp.toJavaInstant()))
+            stmt.setInt(5, PVars.serverId)
         }
     )
 }
