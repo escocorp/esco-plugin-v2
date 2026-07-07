4620f169707f01cd7f5ae19c6c6890e9330ad731
kuko <262641371+kukoldiki@users.noreply.github.com>
2026-07-07 13:18:12 +0500

    upd block cost

--- src/plugin/utils/Gamemode.java
+++ src/plugin/utils/Gamemode.java
@@ -2,14 +2,14 @@ package plugin.utils;
 
 public enum Gamemode {
     idk(),
-    survival("survival", "srv!", 5, 3, 500),
-    sandbox("sandbox", "snd!", 0, 1, 0),
-    attack("attack", "atk!", 0, 1, 1000),
-    pvp("pvp", "pvp!", 0, 1, 600),
-    campaign("campaign", "cmp!", 3, 2, 300),
-    tdefense("tower defense", "td!", 5, 1, 750),
+    survival("survival", "srv!", 5, 50, 500),
+    sandbox("sandbox", "snd!", 0, 10, 0),
+    attack("attack", "atk!", 0, 10, 1000),
+    pvp("pvp", "pvp!", 0, 10, 600),
+    campaign("campaign", "cmp!", 3, 20, 300),
+    tdefense("tower defense", "td!", 5, 10, 750),
     hub("hub", "hb!", 0, 0, 0),
-    hexed("hexed", "hex!", 0, 2, 1050),
+    hexed("hexed", "hex!", 0, 20, 1050),
     crawlerArena("crw", "crw!", 20, 0, 1500);
 
     public boolean optimized;
