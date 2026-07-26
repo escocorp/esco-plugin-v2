c120e6cb476ff9adb3673c4a100038c24b85f4b7
afflictnya <fake@a.com>
2026-07-26 19:47:32 +0300

    fix 2

--- src/plugin/database/models/PlayerData.kt
+++ src/plugin/database/models/PlayerData.kt
@@ -279,11 +279,13 @@ fun getPlayerData(player: Player): PlayerData? {
     if (playerDataCache.containsKey(player)) return playerDataCache.get(player)
     logExpectedCacheMiss(player, "playerDataCache")
 
-    return executeQuery(
+    val pd = executeQuery(
         "SELECT * FROM players WHERE uuid = ?",
         { stmt: PreparedStatement -> stmt.setString(1, player.uuid()) },
         { rs: ResultSet -> getPlayerData(rs) }
     )
+    if (pd != null) playerDataCache.put(player, pd)
+    return pd
 }
 
 fun Player.getData(): PlayerData? {
